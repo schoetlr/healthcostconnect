6ddd22a147fdf067d5300a6c2c11f46af6b95e11
schoetlr <srlrs20020@gmail.com>
2018-10-24 17:01:52 -0400

    remove migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+groups = Procedure.all.chunk { |procedure| procedure.cpt_code }.to_a
+
+#groups of procedures with the same cpt code
+groups.each do |group|
+  #make the provider_procedures all point to one procedure
+  (1...group.length).each do |i|
+    group[i].provider_procedures.each do |pp|
+      pp.procedure_id = group[0].id
+    end
+  end
+
+  #destroy all but the first in a group
+  group.each_with_index do |p, i|
+    next if i == 0
+
+    p.destroy
+  end
+
+
+end
